0f9ab168e5f9f170eb8cd705a173a259a94d1979
Brayden Connole <brayden.connole@gmail.com>
2023-12-06 16:09:24 -0700

    cleanup code

--- day3/challenge3.py
+++ day3/challenge3.py
@@ -2,26 +2,18 @@ import re
 import os
 
 def get_line_sum(sum_line, next_line='', prev_line=''):
-
     total_value = 0
     i = 0
 
     while i < len(sum_line):
         if sum_line[i].isdigit():
-            match = re.search(r'\d+', sum_line[i:])
-            lengthOfNumberFound = match.end()
-            adjacent_start_index = max(0, i-1)
-            adjacent_end_index = min(i +lengthOfNumberFound + 1, len(sum_line))
-
-            adjacent_prev_line = prev_line[adjacent_start_index:adjacent_end_index]
-            adjacent_sum_line = sum_line[adjacent_start_index:adjacent_end_index]
-            adjacent_next_line = next_line[adjacent_start_index:adjacent_end_index]
-
             
-            if has_any_symbol(adjacent_prev_line + adjacent_sum_line + adjacent_next_line):
-                total_value += int(match.group())
+            full_number, length_of_number = get_first_number_and_length(sum_line[i:])
 
-            i += lengthOfNumberFound
+            if is_part(i, length_of_number, sum_line, prev_line, next_line):
+                total_value += full_number
+
+            i += length_of_number
         else:
             i +=1
 
@@ -30,11 +22,26 @@ def get_line_sum(sum_line, next_line='', prev_line=''):
 def has_any_symbol(line):
     return re.search(r'[!@#$%^&*()_+/=-]', line)
 
+def is_part(index, length_of_number, line, prev_line, next_line):
+
+    adjacent_start_index = max(0, index-1)
+    adjacent_end_index = min(index +length_of_number + 1, len(line))
+
+    adjacent_prev_line = prev_line[adjacent_start_index:adjacent_end_index]
+    adjacent_sum_line = line[adjacent_start_index:adjacent_end_index]
+    adjacent_next_line = next_line[adjacent_start_index:adjacent_end_index]
+
+    return has_any_symbol(adjacent_prev_line + adjacent_sum_line + adjacent_next_line)
+
+
+def get_first_number_and_length(line):
+    match = re.search(r'\d+', line)
+    return int(match.group()), match.end()
 
 def get_total_gear_ratio(sum_line, next_line='', prev_line=''):
-
     total_gear_ratio = 0
     stars_indices = []
+
     for match in re.finditer(r'[*]', sum_line):
         stars_indices.append(match.start())
 
@@ -50,19 +57,18 @@ def get_adjacent_numbers(star_index, line1, line2, line3):
     adjacent_start_index = max(0, star_index - 1)
     adjacent_end_index = min(star_index + 1, len(line1))
 
-    for match in re.finditer(r'\d+', line1):
-        if any_part_is_adjacent(match.start(), match.end()-1, adjacent_start_index, adjacent_end_index):
-            adjacent_numbers.append(int(match.group()))
-
-    for match in re.finditer(r'\d+', line2):
-        if any_part_is_adjacent(match.start(), match.end()-1, adjacent_start_index, adjacent_end_index):
-            adjacent_numbers.append(int(match.group()))
-
-    for match in re.finditer(r'\d+', line3):
-        if any_part_is_adjacent(match.start(), match.end()-1, adjacent_start_index, adjacent_end_index):
-            adjacent_numbers.append(int(match.group()))
+    adjacent_numbers += get_numbers_in_range(line1, adjacent_start_index, adjacent_end_index)
+    adjacent_numbers += get_numbers_in_range(line2, adjacent_start_index, adjacent_end_index)
+    adjacent_numbers += get_numbers_in_range(line3, adjacent_start_index, adjacent_end_index)
 
     return adjacent_numbers
+
+def get_numbers_in_range(line, left_index, right_index):
+    numbers = []
+    for match in re.finditer(r'\d+', line):
+        if any_part_is_adjacent(match.start(), match.end()-1, left_index, right_index):
+            numbers += [int(match.group())]
+    return numbers
 
 def any_part_is_adjacent(start, end, left_index, right_index):
         if left_index <= end <= right_index:
