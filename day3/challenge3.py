edfa5829d16d8481c4fccd39f6ba4d3c9bdb9a6d
Brayden Connole <brayden.connole@gmail.com>
2023-12-06 04:38:11 -0700

    2nd challenge solution

--- day3/challenge3.py
+++ day3/challenge3.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 def get_line_sum(sum_line, next_line='', prev_line=''):
 
@@ -6,26 +7,18 @@ def get_line_sum(sum_line, next_line='', prev_line=''):
     i = 0
 
     while i < len(sum_line):
-        # print(i)
         if sum_line[i].isdigit():
             match = re.search(r'\d+', sum_line[i:])
-            # print(match)
-            # print(match.group())
-            # print(match.span())
-            lengthOfNumberFound = match.span()[1]
+            lengthOfNumberFound = match.end()
             adjacent_start_index = max(0, i-1)
-            # print(adjacent_start_index)
             adjacent_end_index = min(i +lengthOfNumberFound + 1, len(sum_line))
 
             adjacent_prev_line = prev_line[adjacent_start_index:adjacent_end_index]
             adjacent_sum_line = sum_line[adjacent_start_index:adjacent_end_index]
             adjacent_next_line = next_line[adjacent_start_index:adjacent_end_index]
-            # print(adjacent_prev_line)
-            # print(adjacent_sum_line)
-            # print(adjacent_next_line)
 
             
-            if has_symbol(adjacent_prev_line + adjacent_sum_line + adjacent_next_line):
+            if has_any_symbol(adjacent_prev_line + adjacent_sum_line + adjacent_next_line):
                 total_value += int(match.group())
 
             i += lengthOfNumberFound
@@ -34,6 +27,81 @@ def get_line_sum(sum_line, next_line='', prev_line=''):
 
     return total_value
 
-def has_symbol(line):
-    return re.search(r'[!@#$%^&*()_+]', line)
+def has_any_symbol(line):
+    return re.search(r'[!@#$%^&*()_+/=-]', line)
 
+
+def get_total_gear_ratio(sum_line, next_line='', prev_line=''):
+
+    total_gear_ratio = 0
+    stars_indices = []
+    for match in re.finditer(r'[*]', sum_line):
+        stars_indices.append(match.start())
+
+    for star_index in stars_indices:
+        numbers = get_adjacent_numbers(star_index, prev_line, sum_line, next_line)
+        if len(numbers) == 2:
+            total_gear_ratio += numbers[0] * numbers[1]
+
+    return total_gear_ratio
+
+def get_adjacent_numbers(star_index, line1, line2, line3):
+    adjacent_numbers = []
+    adjacent_start_index = max(0, star_index - 1)
+    adjacent_end_index = min(star_index + 1, len(line1))
+
+    for match in re.finditer(r'\d+', line1):
+        if any_part_is_adjacent(match.start(), match.end()-1, adjacent_start_index, adjacent_end_index):
+            adjacent_numbers.append(int(match.group()))
+
+    for match in re.finditer(r'\d+', line2):
+        if any_part_is_adjacent(match.start(), match.end()-1, adjacent_start_index, adjacent_end_index):
+            adjacent_numbers.append(int(match.group()))
+
+    for match in re.finditer(r'\d+', line3):
+        if any_part_is_adjacent(match.start(), match.end()-1, adjacent_start_index, adjacent_end_index):
+            adjacent_numbers.append(int(match.group()))
+
+    return adjacent_numbers
+
+def any_part_is_adjacent(start, end, left_index, right_index):
+        if left_index <= end <= right_index:
+            return True
+        elif left_index <= start <= right_index:
+            return True
+
+        return False
+
+if __name__ == "__main__":
+    print('--------------------------Start-----------------------------')
+    print()
+
+    print('Reading file...')
+    file_path = os.path.dirname(__file__) + '/input.txt'
+    print(file_path)
+    print()
+    input_file = open(file_path)
+
+    print('Reading lines in file...')
+    lines = input_file.readlines()
+    print(len(lines), 'lines')
+    print()
+
+    print('calculating puzzles...')
+    first_puzzle_value = 0
+    second_puzzle_value = 0
+    prev_line = ''
+    for i, line in enumerate(lines):
+        if i > 0:
+            prev_line = lines[i-1] 
+
+        if i < len(lines)-1:
+            next_line = lines[i+1]
+        else:
+            next_line = ''
+
+        first_puzzle_value += get_line_sum(line, prev_line=prev_line, next_line=next_line)
+        second_puzzle_value += get_total_gear_ratio(line, prev_line=prev_line, next_line=next_line)
+    
+    print('first_puzzle:', first_puzzle_value)
+    print('second_puzzle:', second_puzzle_value)
--- day3/test_challenge3.py
+++ day3/test_challenge3.py
@@ -1,4 +1,4 @@
-from challenge3 import get_line_sum
+from challenge3 import get_line_sum, get_total_gear_ratio
 
 def test_get_line_sum_zero():
     line1 = '123.....'
@@ -39,3 +39,69 @@ def test_end_lines():
     line1 = '...$.*....'
     line2 = '..64.5.8..'
     assert get_line_sum(line2, prev_line=line1) == 69
+
+def test_get_total_gear_ratio_zero():
+    line1 = '123.....'
+    line2 = '123.....'
+    line3 = '123.....'
+    assert get_total_gear_ratio(line2, prev_line=line1, next_line=line3) == 0
+
+def test_get_total_gear_ratio():
+    line1 = '467..114..'
+    line2 = '...*......'
+    line3 = '..35..633.'
+    assert get_total_gear_ratio(line2, prev_line=line1, next_line=line3) == 467*35
+
+def test_get_total_gear_ratio_zero_matches_with_star():
+    line1 = '4....114..'
+    line2 = '...*......'
+    line3 = '......633.'
+    assert get_total_gear_ratio(line2, prev_line=line1, next_line=line3) == 0
+
+def test_get_total_gear_ratio_one_match_with_star():
+    line1 = '4....114..'
+    line2 = '..2*......'
+    line3 = '......633.'
+    assert get_total_gear_ratio(line2, prev_line=line1, next_line=line3) == 0
+
+def test_get_total_gear_ratio_surround_star():
+    line1 = '4....114..'
+    line2 = '..2*2.....'
+    line3 = '......633.'
+    assert get_total_gear_ratio(line2, prev_line=line1, next_line=line3) == 4
+
+def test_get_total_gear_ratio_star_at_beginning():
+    line1 = '4....114..'
+    line2 = '*.2.2.....'
+    line3 = '3.....633.'
+    assert get_total_gear_ratio(line2, prev_line=line1, next_line=line3) == 12
+
+def test_get_total_gear_ratio_multi_star():
+    line1 = '4......4..'
+    line2 = '*.2.2..*..'
+    line3 = '3......3..'
+    assert get_total_gear_ratio(line2, prev_line=line1, next_line=line3) == 12 + 12
+
+def test_get_total_gear_ratio_same_line():
+    line1 = '4.........'
+    line2 = '..2.5...*.'
+    line3 = '3......2.5'
+    assert get_total_gear_ratio(line2, prev_line=line1, next_line=line3) == 10
+
+def test_get_total_gear_ratio_middle_and_corner():
+    line1 = '4.........'
+    line2 = '..2.5555*.'
+    line3 = '3........1'
+    assert get_total_gear_ratio(line2, prev_line=line1, next_line=line3) == 5555
+
+def test_get_total_gear_ratio_three_directly_under():
+    line1 = '4.........'
+    line2 = '..2*.555..'
+    line3 = '3.111....1'
+    assert get_total_gear_ratio(line2, prev_line=line1, next_line=line3) == 222
+
+def test_get_total_gear_ratio_missing_case():
+    line1 = '......755.'
+    line2 = '...$.*....'
+    line3 = '.664.598..'
+    assert get_total_gear_ratio(line2, prev_line=line1, next_line=line3) ==598*755
