2b2361c50f5ee075c78f84cdbf2acbc530ccf945
Brayden Connole <brayden.connole@gmail.com>
2023-12-08 02:59:17 -0700

    day7 first challenge

---
+++ day7/challenge7.py
@@ -0,0 +1,102 @@
+import os 
+
+CARD_RANK_MAP = {
+    'A': 14,
+    'K': 13,
+    'Q': 12,
+    'J': 11,
+    'T': 10,
+}
+
+CARD_RANK_MAP_JOKERS = {
+    **CARD_RANK_MAP,
+    'J':1,
+}
+
+FIVE_OF_A_KIND = 7
+FOUR_OF_A_KIND = 6
+FULL_HOUSE = 5
+THREE_OF_A_KIND = 4
+TWO_PAIR = 3
+ONE_PAIR = 2
+HIGH_CARD = 1
+
+def get_total_winnings(text, rank_map=CARD_RANK_MAP):
+    hands = extract_data(text, rank_map=rank_map)
+    hands.sort(key=sort_by_rank)
+
+    total_winnings = 0
+    for i, hand in enumerate(hands):
+        total_winnings += hand['bid'] * (i+1)
+    return total_winnings
+
+
+def sort_by_rank(hand):
+    return (hand['rank'], hand['cards'])
+
+def rank_hand(hand):
+    card_counts = {}
+    for card in hand:
+        card_counts[card] = card_counts.get(card, 0) + 1
+
+    counts_of_same_card = list(card_counts.values())
+    counts_of_same_card.sort(reverse=True)
+
+    if counts_of_same_card == [3,2]:
+        return FULL_HOUSE
+    if counts_of_same_card == [2,2,1]:
+        return TWO_PAIR
+
+    highest_count = counts_of_same_card[0]
+    if highest_count == 5:
+        return FIVE_OF_A_KIND
+    if highest_count == 4:
+        return FOUR_OF_A_KIND
+    if highest_count == 3:
+        return THREE_OF_A_KIND
+    if highest_count == 2:
+        return ONE_PAIR
+
+    return HIGH_CARD
+
+def extract_data(text, rank_map=CARD_RANK_MAP):
+    lines = text.split('\n')
+
+    hands = []
+    for line in lines:
+        halves = line.split()
+        cards = halves[0]
+        card_list = []
+        for card in cards:
+            if card.isdigit():
+                card_list += [int(card)]
+            else:
+                card_list += [rank_map[card]]
+
+        hands += [{
+            'cards': card_list,
+            'bid': int(halves[1]),
+            'rank': rank_hand(card_list),
+        }]
+    return hands
+
+if __name__ == "__main__":
+    print('--------------------------Start-----------------------------')
+    print()
+
+    print('Opening file...')
+    file_path = os.path.dirname(__file__) + '/input.txt'
+    print(file_path)
+    print()
+    input_file = open(file_path)
+
+    print('Reading file...')
+    text = input_file.read()
+    print()
+
+    print('calculating puzzles...')
+    first_puzzle_value = get_total_winnings(text)
+    second_puzzle_value = get_total_winnings(text, rank_map=CARD_RANK_MAP_JOKERS)
+
+    print('first puzzle: ', first_puzzle_value)
+    print('second puzzle: ', second_puzzle_value)
---
+++ day7/test_challenge7.py
@@ -0,0 +1,34 @@
+from challenge7 import get_total_winnings, rank_hand, \
+    HIGH_CARD, ONE_PAIR, TWO_PAIR, THREE_OF_A_KIND, FULL_HOUSE, FOUR_OF_A_KIND, FIVE_OF_A_KIND, \
+    CARD_RANK_MAP_JOKERS
+
+TEST_INPUT = """
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+""".strip()
+
+
+def test_get_total_winnings():
+    assert get_total_winnings(TEST_INPUT) == 6440
+
+
+def test_rank_hand():
+    assert rank_hand([3,3,3,3,3]) == FIVE_OF_A_KIND
+    assert rank_hand([3,3,3,1,3]) == FOUR_OF_A_KIND
+    assert rank_hand([3,3,3,1,2]) == THREE_OF_A_KIND
+    assert rank_hand([3,2,10,3,13]) == ONE_PAIR
+    assert rank_hand([3,2,10,9,13]) == HIGH_CARD
+    assert rank_hand([5,5,5,2,2]) == FULL_HOUSE
+    assert rank_hand([5,5,2,2,2]) == FULL_HOUSE
+    assert rank_hand([5,5,11,2,2]) == TWO_PAIR
+
+
+def test_get_total_winnings_with_jokers():
+    assert get_total_winnings(TEST_INPUT, rank_map=CARD_RANK_MAP_JOKERS) == 5905
+
+def test_rank_hand_with_jokers():
+    assert rank_hand([1,1,1,1,1]) == FIVE_OF_A_KIND
+    assert rank_hand([2,2,2,2,1]) == FIVE_OF_A_KIND
